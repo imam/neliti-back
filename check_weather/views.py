03aebb309f47f9d5b94f7ecae70a2af74508f4da
Imam Assidiqqi <assidiqqi.imam@gmail.com>
2021-06-03 09:26:20 +0700

    Forward lat and lon to the api and return response

--- check_weather/views.py
+++ check_weather/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+import requests
+import logging
 # Create your views here.
+
+def index(request):
+    lat = request.GET.get('lat')
+    lon = request.GET.get('lon')
+    log = logging.getLogger("my-logger")
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64;"}
+    r = requests.get(f'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat={lat}&lon={lon}', headers=headers)
+    print(r.content)
+    return JsonResponse(r.json())
